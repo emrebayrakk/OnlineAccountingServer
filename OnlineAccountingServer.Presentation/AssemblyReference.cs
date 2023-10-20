0bff6c8953c8fc577f477135f1d83013841ea9f8
emrebayrak <eemmrree0@outlook.com>
2023-10-20 16:48:00 +0300

    Add project files.

---
+++ OnlineAccountingServer.Presentation/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace OnlineAccountingServer.Presentation
+{
+    public static class AssemblyReference
+    {
+        public readonly static Assembly Assembly = typeof(Assembly).Assembly;
+    }
+}
